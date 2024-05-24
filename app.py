6dddf1751ee1989a7e8052910048746266c3876d
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-05-24 18:20:24 +0200

    Avancer

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from googletrans import Translator, LANGUAGES
 
 app = Flask(__name__)
+translator = Translator()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    languages = LANGUAGES
+    return render_template('index.html', languages=languages)
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    text_to_translate = request.form['text']
+    dest_language = request.form['language']
+    translated = translator.translate(text_to_translate, dest=dest_language)
+    return render_template('result.html', original=text_to_translate, translation=translated.text, dest_language=dest_language)
 
 if __name__ == '__main__':
     app.run(debug=True)
